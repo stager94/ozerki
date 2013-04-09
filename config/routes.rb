f82ea8ef4eb5763a9c960e222fc5323f2316d8de
stager <stager@stager-PC.(none)>
2013-04-10 00:01:08 +0300

    - some fixes 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Hostel::Application.routes.draw do
-  mount Ckeditor::Engine => '/ckeditor'
-  
   ActiveAdmin.routes(self)
-
   devise_for :admin_users, ActiveAdmin::Devise.config
 
+  mount Ckeditor::Engine => '/ckeditor'
+  
   root to: "application#index"
 
   resources :news
