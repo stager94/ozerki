a33ff5c3f961a4cb7fb27249fdcf84aeaff74a3f
stager <stager@stager-PC.(none)>
2013-04-09 18:31:32 +0300

    My initial commit message

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hostel::Application.config.secret_token = '1776a252cde6a670da66637586476d83ceee434a1bf529520a134fd49358ffc0600042eaeff4811f15672b60872f371cbe2457faa791ddf393937eecfc1fa503'
