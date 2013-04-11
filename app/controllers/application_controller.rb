31c8bb0095fa918625c5f346aa381e6a27bd07f2
stager <stager@stager-PC.(none)>
2013-04-11 23:22:32 +0300

    - add breadcrumbs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :settings
+  add_breadcrumb I18n.t('pages.home'), :root_path
 
   def index
   	@news = New.root
--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,14 +1,18 @@
 class NewsController < ApplicationController
 	before_filter :find_new, only: [:show]
 	before_filter :settings
+	add_breadcrumb I18n.t('pages.news'), :news_index_path
+
 	def index
-		@news = New.displayed
+		@news   = New.displayed
+		@h1     = t('pages.news')
 	end
 
 	def show
 		@meta_description  = @new.snippet
 		@meta_keywords     = @new.tag_list
 		@meta_author       = @new.author
+		add_breadcrumb @new.title, news_path(@new)
 	end
 
 	protected
