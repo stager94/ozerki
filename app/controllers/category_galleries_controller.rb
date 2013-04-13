8e4b9bafbadf71b344ea84fea6b3715f0b790ef2
stager <stager@stager-PC.(none)>
2013-04-14 00:36:29 +0300

    - gallery made qqq

--- app/controllers/category_galleries_controller.rb
+++ app/controllers/category_galleries_controller.rb
@@ -1,6 +1,22 @@
 class CategoryGalleriesController < ApplicationController
+	before_filter :private_settings
+
 	def index
 		@categories = CategoryGallery.displayed
 		@all_categories = CategoryGallery.main
+		@h1 = I18n.t 'pages.gallery'
+	end
+
+	def show
+		@category = CategoryGallery.find(params[:id])
+		@all_categories = CategoryGallery.main
+		@categories = @category.children
+		add_breadcrumb @category.title, @category
+		@h1 = @category.title
+	end
+
+	private
+	def private_settings
+		add_breadcrumb I18n.t 'pages.gallery', category_galleries_path
 	end
 end
