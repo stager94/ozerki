2f2bd45ab1312726ecacdd3895b0be370f46b176
stager <stager@stager-PC.(none)>
2013-04-14 00:26:59 +0300

    - gallery make

--- app/helpers/category_galleries_helper.rb
+++ app/helpers/category_galleries_helper.rb
@@ -1,2 +1,22 @@
 module CategoryGalleriesHelper
+	def get_params_to_page(category, all_categories)
+		res = Array.new
+		return_res = Hash.new
+		
+		all_categories.each do |cat|
+			res << cat if cat.root == category
+		end
+
+		category_image = Gallery.where('category_gallery_id IN (?)', res).order('RANDOM()')
+		
+		count = 0
+		res.each do |cat|
+			count = count+cat.galleries_count
+		end
+
+		return_res['count']  = count
+		return_res['images'] = category_image
+
+		return return_res
+	end
 end
