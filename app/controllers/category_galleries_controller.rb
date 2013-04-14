e42df50a26d65001751e40d99e67c8719c1c5692
stager <stager@stager-PC.(none)>
2013-04-14 10:57:41 +0300

    - gallery logic

--- app/controllers/category_galleries_controller.rb
+++ app/controllers/category_galleries_controller.rb
@@ -1,22 +1,36 @@
 class CategoryGalleriesController < ApplicationController
 	before_filter :private_settings
+	before_filter :get_category, only: [:show]
+	before_filter :set_breadcrumb_tree, only: [:show]
+
+	
 
 	def index
 		@categories = CategoryGallery.displayed
-		@all_categories = CategoryGallery.main
+
 		@h1 = I18n.t 'pages.gallery'
 	end
 
 	def show
-		@category = CategoryGallery.find(params[:id])
-		@all_categories = CategoryGallery.main
-		@categories = @category.children
-		add_breadcrumb @category.title, @category
+		@photos = Gallery.where('category_gallery_id = ?', @category)
+
 		@h1 = @category.title
 	end
 
 	private
 	def private_settings
-		add_breadcrumb I18n.t 'pages.gallery', category_galleries_path
+		add_breadcrumb I18n.t('pages.gallery'), :category_galleries_path
+		@all_categories = CategoryGallery.main
+	end
+
+	def get_category
+		@category = CategoryGallery.find(params[:id])
+		@categories = @category.children
+	end
+
+	def set_breadcrumb_tree
+		@category.self_and_ancestors.each do |category|
+			add_breadcrumb category.title, category_gallery_path(category)
+		end
 	end
 end
