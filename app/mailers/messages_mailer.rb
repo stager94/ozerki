f6db070da79cac2cadbc74ee9c7a80c1bfeec3da
stager <stager@stager-PC.(none)>
2013-06-13 15:26:32 +0300

    - Fix bug and add style to alertify

--- app/mailers/messages_mailer.rb
+++ app/mailers/messages_mailer.rb
@@ -2,14 +2,14 @@ class MessagesMailer < ActionMailer::Base
   default to: "aho@skmz.dn.ua"
 
   def book(params)
-    @name   = params[:name]
-    @phone  = params[:phone]
-    @email  = params[:email]
-    @from   = params[:from]
-    @to     = params[:to]
-    @places = params[:places]
-    @to     = Configure.first[:book_email]
+    @name          = params[:name]
+    @phone         = params[:phone]
+    @email         = params[:email]
+    @from          = params[:from]
+    @to            = params[:to]
+    @places        = params[:places]
+    @recipient     = Configure.first[:book_email]
 
-  	mail to: @to, from: @email, subject: I18n.t('masseges_mailer.subjects.booking')
+  	mail to: @recipient, from: @email, subject: I18n.t('masseges_mailer.subjects.booking')
   end
 end
