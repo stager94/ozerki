7ca83f4e18769f45eeeaa2df025e6f0cd4f1db09
stager <stager@stager-PC.(none)>
2013-06-26 21:13:09 +0300

    - Change Content Type

--- app/models/configure.rb
+++ app/models/configure.rb
@@ -1,4 +1,5 @@
 class Configure < ActiveRecord::Base
   attr_accessible :email, :module_news_per_page, :news_per_page, :site_title, :title_separator, :site_keywords, :site_snippet, :news_id, :book_email, :book_days_num, :book_excel
-  has_attached_file :book_excel
+  has_attached_file :book_excel, keep_old_files: false
+  validates_attachment_content_type :book_excel, content_type: ["application/vnd.ms-excel"]
 end
