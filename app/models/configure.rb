56ef69ecfc40a8145dfda7e699b59e08408c07c8
stager <stager@stager-PC.(none)>
2013-06-26 21:31:14 +0300

    - xlsx Content Type Added

--- app/models/configure.rb
+++ app/models/configure.rb
@@ -1,5 +1,6 @@
 class Configure < ActiveRecord::Base
   attr_accessible :email, :module_news_per_page, :news_per_page, :site_title, :title_separator, :site_keywords, :site_snippet, :news_id, :book_email, :book_days_num, :book_excel
   has_attached_file :book_excel, keep_old_files: false
-  validates_attachment_content_type :book_excel, content_type: ["application/vnd.ms-excel"]
+  validates_attachment_content_type :book_excel, content_type: ["application/vnd.ms-excel", 
+  	"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"]
 end
