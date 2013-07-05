b03cd1a270136b4e8183746adf5ab8864ed59935
stager <stager@stager-PC.(none)>
2013-07-06 00:21:01 +0300

    - Get all pages

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,8 +1,12 @@
 class Api::PagesController < ApplicationController
 	def index
-		term = params[:data]
-		pages = Page.where("lower_title LIKE ?", "%#{term.mb_chars.downcase.to_s}%")
-		
+		if params[:data].present?
+			term = params[:data] 
+			pages = Page.where("lower_title LIKE ?", "%#{term.mb_chars.downcase.to_s}%")
+		else
+			pages = Page.all
+		end
+
 		render json: pages
 	end
 end
