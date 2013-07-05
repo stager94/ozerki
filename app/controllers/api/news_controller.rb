b10539887e83c6d64739e979f40edf24d773bd6c
stager <stager@stager-PC.(none)>
2013-07-06 00:30:49 +0300

    - Change conditionals

--- app/controllers/api/news_controller.rb
+++ app/controllers/api/news_controller.rb
@@ -3,9 +3,9 @@ class Api::NewsController < ApplicationController
 	def index
 		if params[:data].present?
 			term = params[:data]
-			news = New.where("LOWER(lower_title) LIKE ?", "%#{term.mb_chars.downcase.to_s}%")
+			news = New.where("LOWER(lower_title) LIKE ?", "%#{term.mb_chars.downcase.to_s}%").select("id, title, lower_title")
 		else
-			news = New.all
+			news = New.find(:all, select: "id, title, lower_title")
 		end
 
 		render json: news
--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -2,9 +2,9 @@ class Api::PagesController < ApplicationController
 	def index
 		if params[:data].present?
 			term = params[:data] 
-			pages = Page.where("lower_title LIKE ?", "%#{term.mb_chars.downcase.to_s}%")
+			pages = Page.where("lower_title LIKE ?", "%#{term.mb_chars.downcase.to_s}%").select("id, title, lower_title")
 		else
-			pages = Page.all
+			pages = Page.find(:all, select: "id, title, lower_title")
 		end
 
 		render json: pages
