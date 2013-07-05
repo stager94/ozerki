182127409300ddbb88242e9e0f4287c6a61b2693
stager <stager@stager-PC.(none)>
2013-07-06 00:21:46 +0300

    - Get all news

--- app/controllers/api/news_controller.rb
+++ app/controllers/api/news_controller.rb
@@ -1,8 +1,12 @@
 # coding: utf-8
 class Api::NewsController < ApplicationController
 	def index
-		term = params[:data]
-		news = New.where("LOWER(lower_title) LIKE ?", "%#{term.mb_chars.downcase.to_s}%")
+		if params[:data].present?
+			term = params[:data]
+			news = New.where("LOWER(lower_title) LIKE ?", "%#{term.mb_chars.downcase.to_s}%")
+		else
+			news = New.all
+		end
 
 		render json: news
 	end
